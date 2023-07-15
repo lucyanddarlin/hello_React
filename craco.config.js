d559bb3d9e56946069035c6e4579b478d844d6d7
lucy.li <lucy.li@quectel.com>
2023-07-15 11:56:41 +0800

    init

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const CracoAntDesignPlugin = require("craco-antd");
+module.exports = {
+  plugins: [{ plugin: CracoAntDesignPlugin }],
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
